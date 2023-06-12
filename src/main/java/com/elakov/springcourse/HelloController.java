04e3cc4858f3431a771d0994bf178e44b2ee5805
nikitaelakov <nelakov24@gmail.com>
2023-06-12 20:42:41 +0400

    lesson-1: FirstApp

---
+++ src/main/java/com/elakov/springcourse/HelloController.java
@@ -0,0 +1,13 @@
+package com.elakov.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
