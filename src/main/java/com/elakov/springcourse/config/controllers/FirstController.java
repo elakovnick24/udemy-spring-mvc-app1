d8e9460a363c48782e87c937524e2503f11c5324
nikitaelakov <nelakov24@gmail.com>
2023-06-12 23:44:08 +0400

    lesson-4: Request Parameters

--- src/main/java/com/elakov/springcourse/config/controllers/FirstController.java
+++ src/main/java/com/elakov/springcourse/config/controllers/FirstController.java
@@ -3,6 +3,9 @@ package com.elakov.springcourse.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Neil Alishev
@@ -12,7 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//        System.out.println("Hello, " + name + " " + surname);
+//        return "first/hello";
+//    }
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
