10140bf767645dae512691abf88f09312aa1a31d
nikitaelakov <nelakov24@gmail.com>
2023-06-13 23:30:03 +0400

    lesson-7: PATCH and DELETE (

--- src/main/java/com/elakov/springcourse/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/com/elakov/springcourse/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,6 +1,10 @@
 package com.elakov.springcourse.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 /**
  * @author Neil Alishev
@@ -20,4 +24,16 @@ public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotation
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+
 }
