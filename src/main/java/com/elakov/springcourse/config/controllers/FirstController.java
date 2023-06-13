a70ee3e4126413dfb5ba0dd669d5905f7056bc72
nikitaelakov <nelakov24@gmail.com>
2023-06-13 19:26:25 +0400

    homework-5: Calculator

--- src/main/java/com/elakov/springcourse/config/controllers/FirstController.java
+++ src/main/java/com/elakov/springcourse/config/controllers/FirstController.java
@@ -29,4 +29,33 @@ public class FirstController {
     public String goodByePage() {
         return "first/goodbye";
     }
+
+    @GetMapping("/calculator" )
+    public String calculator(@RequestParam("a") int a,
+                            @RequestParam(value = "b") int b,
+                            @RequestParam(value = "action") String action,
+                            Model model) {
+        double result;
+        switch (action) {
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double)b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", "Total " + action + " = " + result);
+        return "first/calculate.html";
+    }
+
 }
