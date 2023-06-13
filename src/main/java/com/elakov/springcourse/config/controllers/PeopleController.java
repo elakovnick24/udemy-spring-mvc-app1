0a5857be0700bacb498e59e16e3ae3a44e12fe04
nikitaelakov <nelakov24@gmail.com>
2023-06-13 20:02:28 +0400

    lesson-6: Pattern DAO and Thymeleaf

---
+++ src/main/java/com/elakov/springcourse/config/controllers/PeopleController.java
@@ -0,0 +1,34 @@
+package com.elakov.springcourse.config.controllers;
+
+import com.elakov.springcourse.config.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        // Get all peoples from DAO and sent to view
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // Get one people for id from DAO and sent to view
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
---
+++ src/main/java/com/elakov/springcourse/config/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package com.elakov.springcourse.config.dao;
+
+import com.elakov.springcourse.config.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
