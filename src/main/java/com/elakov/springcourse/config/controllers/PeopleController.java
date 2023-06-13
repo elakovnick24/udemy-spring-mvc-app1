c8633aff495269fc539f6e3da97fa0e7cd3bfaf7
nikitaelakov <nelakov24@gmail.com>
2023-06-13 21:27:35 +0400

    lesson-6: Model Attribute annotation

--- src/main/java/com/elakov/springcourse/config/controllers/PeopleController.java
+++ src/main/java/com/elakov/springcourse/config/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package com.elakov.springcourse.config.controllers;
 
 import com.elakov.springcourse.config.dao.PersonDAO;
+import com.elakov.springcourse.config.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -14,6 +13,7 @@ public class PeopleController {
 
     private final PersonDAO personDAO;
 
+    @Autowired
     public PeopleController(PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
@@ -31,4 +31,18 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+
 }
--- src/main/java/com/elakov/springcourse/config/models/Person.java
+++ src/main/java/com/elakov/springcourse/config/models/Person.java
@@ -4,6 +4,10 @@ public class Person {
     private int id;
     private String name;
 
+    public Person() {
+
+    }
+
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
