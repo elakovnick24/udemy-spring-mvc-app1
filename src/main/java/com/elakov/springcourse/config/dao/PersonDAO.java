762a7ac752d5c011f763cf842895dd6d97f1bfaf
nikitaelakov <nelakov24@gmail.com>
2023-06-14 23:13:11 +0400

    lesson-12: Configuration DB from external file.properties

--- src/main/java/com/elakov/springcourse/config/dao/PersonDAO.java
+++ src/main/java/com/elakov/springcourse/config/dao/PersonDAO.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 @Component
